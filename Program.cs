0edd836fb10002063987233380238263737ff376
Faruk Esen <farukesen@192.168.1.12>
2025-01-30 11:48:24 +0300

    New console project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sayilar sayilar = new Sayilar();
+        List<int> sayiListesi = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        
+        Console.WriteLine("Tüm sayılar:");
+        foreach (int sayi in sayiListesi)
+        {
+            Console.Write(sayi + " ");
+        }
+        Console.WriteLine();
+        
+        List<int> ciftSayilar = sayilar.CiftSayilariBul(sayiListesi);
+        
+        Console.WriteLine("\nÇift sayılar:");
+        foreach (int ciftSayi in ciftSayilar)
+        {
+            Console.Write(ciftSayi + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Sayilar.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Sayilar
+{
+    public List<int> CiftSayilariBul(List<int> sayilar)
+    {
+        List<int> ciftSayilar = new List<int>();
+        
+        foreach (int sayi in sayilar)
+        {
+            if (sayi % 2 == 0)
+            {
+                ciftSayilar.Add(sayi);
+            }
+        }
+        
+        return ciftSayilar;
+    }
+} 
